ef269459899f4c9d519949f8a59b5515d2c5f704
rubens <rubsandsu@gmail.com>
2019-07-24 15:26:16 -0300

    adicionando protótipo tela delete interv

--- src/Components/Manager/Delete.js
+++ src/Components/Manager/Delete.js
@@ -2,6 +2,96 @@ import React from "react";
 
 // import { Container } from './styles';
 
-export default function Delete() {
-  return <div />;
+export default function Delete(props) {
+  //const {interventions} = props;
+  const [interventions, setInterventions] = React.useState([
+    {
+      responsable: "cagece",
+      data_ini: "12/07/2019",
+      data_fim: "13/07/2019",
+      description: "its a intervention!"
+    },
+    {
+      responsable: "cegas",
+      data_ini: "12/07/2019",
+      data_fim: "13/07/2019",
+      description: "its a another intervention!"
+    },
+    {
+      responsable: "cagece",
+      data_ini: "12/07/2019",
+      data_fim: "13/07/2019",
+      description: "its a intervention!"
+    }
+  ]);
+
+  const [chosen, setChosen] = React.useState(<div />);
+  const [indexChosen, setIndexChosen] = React.useState(<div />);
+
+  const prettifyEl = el => (
+    <div className="border-left pl-2 m-2">
+      <div>
+        <span style={{fontWeight: "bold"}}>Responsável: </span>
+        {el.responsable}
+      </div>
+      <div>
+        <span style={{fontWeight: "bold"}}>data início: </span>
+        {el.data_ini}
+      </div>
+      <div>
+        <span style={{fontWeight: "bold"}}>data fim: </span>
+        {el.data_fim}
+      </div>
+    </div>
+  );
+
+  const linkClicked = (el, index) => {
+    console.log("link clicked");
+    document.getElementById("chosen").style.display = "block";
+    setChosen(prettifyEl(el));
+    setIndexChosen(index);
+  };
+
+  const mapInterventions = interventions.map((el, index) => {
+    return (
+      <a
+        key={index}
+        onClick={e => {
+          e.preventDefault();
+          linkClicked(el, index);
+        }}
+        href={index}
+      >
+        {prettifyEl(el)}
+      </a>
+    );
+  });
+
+  return (
+    <>
+      <div class="newdropdown">
+        {/* <button class="newdropbtn">Dropdown</button> */}
+        <div className="newdropbtn border rounded">Intervenções</div>
+        <div class="newdropdown-content">{mapInterventions}</div>
+      </div>
+      <div id="chosen" style={{display: "none"}}>
+        <div className="container  p-2 m-2">{chosen}</div>
+        <button
+          onClick={() => {
+            let doidim = [];
+            interventions.forEach((el, index) => {
+              if (index !== indexChosen) {
+                doidim.push(el);
+              }
+            });
+            setInterventions(doidim);
+            document.getElementById("chosen").style.display = "none";
+          }}
+          className="p-2 m-2 ml-auto"
+        >
+          Exluir Intervenção
+        </button>
+      </div>
+    </>
+  );
 }
--- src/Pages/Testes/teste1.js
+++ src/Pages/Testes/teste1.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Create from "../../Components/Manager/Create";
+import Delete from "../../Components/Manager/Delete";
 
 // import { Container } from './styles';
 
@@ -29,6 +30,33 @@ const teste1 = props => {
     return mapp;
   };
 
+  const buttonClicked = who => {
+    let divv;
+    switch (who) {
+      case "create": {
+        divv = "create";
+        break;
+      }
+      case "update": {
+        divv = "update";
+        break;
+      }
+      case "delete": {
+        divv = "delete";
+        break;
+      }
+      default: {
+        console.log("erro no button clicked");
+        break;
+      }
+    }
+
+    document.getElementById("create").style.display = "none";
+    document.getElementById("update").style.display = "none";
+    document.getElementById("delete").style.display = "none";
+    document.getElementById(divv).style.display = "block";
+  };
+
   return (
     <>
       <div class="row">
@@ -39,6 +67,10 @@ const teste1 = props => {
         </div>
       </div>
 
+      <button onClick={() => buttonClicked("create")}>Create</button>
+      <button onClick={() => buttonClicked("update")}>Update</button>
+      <button onClick={() => buttonClicked("delete")}>Delete</button>
+
       <div className="row">
         <div className="col-lg-6 border-right ml-auto">
           <p>
@@ -47,8 +79,14 @@ const teste1 = props => {
               : agua[key]}
           </p>
         </div>
-        <div className="col-lg-6 mb-5">
+        <div id="create" className="col-lg-6 mb-5">
           <Create onSubmit={e => console.log("submeteu", e)} />
+        </div>
+        <div id="update" display="none" className="col-lg-6 mb-5">
+          <p>UPDATE</p>
+        </div>
+        <div id="delete" display="none" className="col-lg-6 mb-5">
+          <Delete interventions={[1, 2, 3, 4, 5]} />
         </div>
       </div>
     </>
